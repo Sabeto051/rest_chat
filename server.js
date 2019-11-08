9b9d2c915c5683007858e0394be17ed9dd901312
Nicolas <pereznicolas90@gmail.com>
2019-11-07 19:44:34 -0500

    Listo el login xdd

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 // const path = require('path')
 const { mongoose } = require('./database')
+const router = express.Router();
+var bodyParser = require('body-parser')
 
 const app = express()
 
 // Settings
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3030)
 
 // Middlewares
 app.use(express.json())
@@ -13,9 +15,21 @@ app.use(express.json())
 app.use('/api/messages', require('./routes/message.routes'))
 
 //functions
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('Welcome to the Chat Server')
 })
+
+app.post("/", bodyParser.urlencoded({ extended: false }), function (req, res, next) {
+  var id = req.body.id;
+  var pw = req.body.pw;
+
+  if (id == "test" && pw == "1234") {
+    res.send("Welcome to the Chat Server")
+  }
+  else {
+    res.send("Wrong username or password, try again")
+  }
+});
 
 // Starting the server
 app.listen(app.get('port'), () => {
