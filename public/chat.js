6dcfc4dc44dac35a6f3029c8b929b34e8b247b84
Sabeto051 <bedoya.santi@gmail.com>
2019-11-09 22:50:31 -0500

    cliente terminado

--- public/chat.js
+++ public/chat.js
@@ -5,34 +5,74 @@ let btn = document.getElementById('send')
 let output = document.getElementById('output')
 let actions = document.getElementById('actions')
 
-/*
-  Esto  se usa, pero hay que adaptarlo a rest, puesto que está en socket:
-*/
+btn.addEventListener('click', send_message)
+message.addEventListener('keypress', enter_pressed)
 
-// btn.addEventListener('click', () => {
-//   data = {
-//     username: username.value,
-//     message: message.value
-//   }
-//   socket.emit('chat:message', data)
-// })
+function enter_pressed(event) {
+  if (event.keyCode == 13) {
+    send_message()
+  }
+}
 
-// message.addEventListener('keypress', () => {
-//   socket.emit('chat:typing', username.value)
-// })
+function send_message() {
+  if (username.value !== '' && message.value !== '') {
+    let data = {
+      username: username.value,
+      message: message.value
+    }
 
-// socket.on('chat:message', data => {
-//   actions.innerHTML = ''
+    fetch('/send', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    output.innerHTML += `<p>
+          <strong>${data.username}</strong>: ${data.message}
+      </p>`
+    output.scrollTop = output.scrollHeight
+    message.value = ''
+  }
+}
 
-//   output.innerHTML += `<p>
-//           <strong>${data.username}</strong>: ${data.message}
-//       </p>`
-// })
+function print_historial(data) {
+  let toBottom = false
+  if (output.scrollTop === output.scrollHeight - 400) {
+    toBottom = true
+    console.log(toBottom)
+  }
+  output.innerHTML = ''
+  data.forEach(mensaje => {
+    output.innerHTML += `<p>
+          <strong>${mensaje.owner}</strong>: ${mensaje.content}
+      </p>`
+  })
 
-// socket.on('chat:typing', data => {
-//   if (data === '' || data === null) {
-//     actions.innerHTML = `<p><em>-Stranger- is typing a message.</em></p>`
-//   } else {
-//     actions.innerHTML = `<p><em>${data} is typing a message.</em></p>`
-//   }
-// })
+  if (toBottom) {
+    output.scrollTop = output.scrollHeight
+  }
+  // console.log('output.scrollHeight' + output.scrollHeight)
+  // console.log('output.scrollTop' + output.scrollTop)
+}
+
+fetch('/history')
+  .then(res => res.json())
+  .then(data => {
+    print_historial(data)
+  })
+  .catch(err => {
+    console.error(err)
+  })
+
+setInterval(() => {
+  fetch('/history')
+    .then(res => res.json())
+    .then(data => {
+      print_historial(data)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+}, 1000)
--- server.js
+++ server.js
@@ -1,18 +1,56 @@
 const path = require('path')
 const express = require('express')
+const request = require('request')
 const app = express()
 
 // settings
-app.set('port', process.env.PORT || 3002)
+app.set('port', process.env.PORT || 3000)
 
 app.use(express.json())
 
 // static files
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.post('/aaa', (req, res) => {
-  // console.log(req)
-  console.log(req.body)
+app.post('/send', (req, res) => {
+  var body = JSON.stringify({
+    owner: req.body.username,
+    content: req.body.message
+  })
+  var options = {
+    method: 'POST',
+    encoding: 'utf-8',
+    url: 'http://172.105.22.206:3000/api/messages/create',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: body
+  }
+  request(options, (error, response, body) => {
+    if (error) {
+      console.log(error)
+    } else {
+      // console.log(JSON.parse(body))
+      res.send(body)
+    }
+  })
+})
+
+app.get('/history', (req, res) => {
+  var options = {
+    method: 'GET',
+    encoding: 'utf-8',
+    url: 'http://172.105.22.206:3001/api/messages/history'
+  }
+
+  request(options, (error, response, body) => {
+    if (error) {
+      console.log(error)
+    } else {
+      // let history = JSON.parse(body)
+      // console.log(body)
+      res.send(body)
+    }
+  })
 })
 
 app.listen(app.get('port'), () => {
